652ccbf709a2878b8aab312205bfe1278b9f4294
Dokkabei97 <wkdrn970@naver.com>
2022-03-12 19:52:27 +0900

    초기 설정

--- lib/main.dart
+++ lib/main.dart
@@ -17,9 +17,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Container(
-
-      ),
+      home: Container(),
     );
   }
 }
